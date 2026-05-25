b144887e1414bd9fb19cd8221d7918aa01a5f39a
Trevor Bercher <trevorbercher@gmail.com>
2026-05-25 17:25:57 -0400

    Feat: Created TaskRepository interface

--- src/main/java/com/trevorbercher/taskmanager/entity/Task.java
+++ src/main/java/com/trevorbercher/taskmanager/entity/Task.java
@@ -1,22 +1,39 @@
 package com.trevorbercher.taskmanager.entity;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
-
+@Entity
+@Table(name = "tasks")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-
 public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false)
     private String title;
+
+    @Column(length = 500)
     private String description;
-    private Boolean completed;
+
+    @Column(nullable = false)
+    private Boolean completed = false;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
     private LocalDateTime createdAt;
+
+    @PrePersist
+    protected void onCreate(){
+        this.createdAt = LocalDateTime.now();
+    }
 
 
 }
