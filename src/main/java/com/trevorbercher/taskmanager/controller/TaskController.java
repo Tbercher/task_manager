85bf6b9736e7b553b1ba4eb39d758910fc6e1a8a
Trevor Bercher <trevorbercher@gmail.com>
2026-05-25 20:33:57 -0400

    Feat: Refactored Code
    Refactored code to divide logical operations between the three classes.
    
    Controller: Handles HTTP, Validates inputs, Returns Responses
    
    Service: Handle Business Logic, Overall Orchestration, And Transactions
    
    Repository: Handles Database Operations.

--- src/main/java/com/trevorbercher/taskmanager/controller/TaskController.java
+++ src/main/java/com/trevorbercher/taskmanager/controller/TaskController.java
@@ -1,14 +1,12 @@
 package com.trevorbercher.taskmanager.controller;
 
 import com.trevorbercher.taskmanager.entity.Task;
-import com.trevorbercher.taskmanager.repository.TaskRepository;
+import com.trevorbercher.taskmanager.service.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -16,52 +14,54 @@ import java.util.List;
 public class TaskController {
 
 
-    private final TaskRepository taskRepository;
+    private final TaskService taskService;
 
     //constructor injection
     @Autowired
-    public TaskController(TaskRepository taskRepository){
-        this.taskRepository = taskRepository;
+    public TaskController(TaskService taskService){
+        this.taskService = taskService;
     }
 
     @GetMapping
     public List<Task> getAllTasks(){
-        return taskRepository.findAll();
+        return taskService.getAllTasks();
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Task> getTaskById(@PathVariable Long id){
-        return taskRepository.findById(id)
+        return taskService.getTaskById(id)
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
     }
 
     @PostMapping
     public ResponseEntity<Task> createTask(@RequestBody Task task){
-        Task savedTask = taskRepository.save(task);
+        Task savedTask = taskService.createTask(task);
         return ResponseEntity.status(HttpStatus.CREATED).body(savedTask);
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<Task> updateTask(@PathVariable Long id, @RequestBody Task updatedTask){
-        return taskRepository.findById(id)
-                .map(task ->{
-                    task.setTitle(updatedTask.getTitle());
-                    task.setDescription(updatedTask.getDescription());
-                    task.setCompleted(updatedTask.getCompleted());
-                    Task savedTask = taskRepository.save(task);
-
-                    return ResponseEntity.ok(savedTask);
-                })
+        return taskService.updateTask(id, updatedTask)
+                .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
     }
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteTask(@PathVariable Long id){
-        return taskRepository.findById(id)
-                .map(task -> {
-                    taskRepository.delete(task);
-                    return ResponseEntity.ok().<Void>build();
-                })
-                .orElse(ResponseEntity.notFound().build());
+        return taskService.deleteTask(id) ?
+                ResponseEntity.ok().build() :
+                ResponseEntity.notFound().build();
+
     }
+
+    @GetMapping("/completed/{status}")
+    public List<Task> getTasksByCompletions(@PathVariable boolean status){
+        return taskService.getTasksByCompletionStatus(status);
+    }
+
+    @GetMapping("/search")
+    public List<Task> searchTasksByTitle(@RequestParam String title){
+        return taskService.searchTasksByTitle(title);
+    }
+
 }
--- src/main/java/com/trevorbercher/taskmanager/repository/TaskRepository.java
+++ src/main/java/com/trevorbercher/taskmanager/repository/TaskRepository.java
@@ -2,8 +2,22 @@ package com.trevorbercher.taskmanager.repository;
 
 import com.trevorbercher.taskmanager.entity.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
+
+    // Method name query
+    // Translated SELECT * FROM tasks WHERE completed = :completed
+    List<Task> findByCompleted(boolean completed);
+
+    List<Task> findByTitleContainingIgnoreCase(String title);
+
+    @Query("SELECT t FROM Task t Where t.completed = :completed")
+    List<Task> findTasksByCompletionStatus(@Param("completed") boolean completed);
+
 }
